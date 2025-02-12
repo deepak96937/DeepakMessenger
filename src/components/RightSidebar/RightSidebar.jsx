b3dde15b4ed9bd500f5876d1bcd1f78a639eea6f
Your Name <your-email@example.com>
2025-02-12 22:44:30 +0530

    add RightSidebar component

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import './RightSidebar.css'
+import React from "react";
+import "./RightSidebar.css";
+import assets from "../../assets/assets";
 
 const RightSidebar = () => {
   return (
-    <div>
-      right
-    </div>
-  )
-}
+    <div className="rs">
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>
+          Richard Sandord <img src={assets.green_dot} className="dot" alt="" />
+        </h3>
+        <p>Hey, There i am using chat app</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic3} alt="" />
+        </div>
+      </div>
 
-export default RightSidebar
+      <button>Logout</button>
+    </div>
+  );
+};
+
+export default RightSidebar;
